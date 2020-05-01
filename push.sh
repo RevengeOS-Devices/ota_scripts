726a02fce04783d5f386dd245ca27e783742febd
nysascape <jago@nysascape.digital>
2020-05-01 16:51:00 +0000

    push: Finalise it?
    Change-Id: Icbfa3b42c2fbf27244dbaf160c46f205c54e98b7
    Signed-off-by: nysascape <jago@nysascape.digital>

--- push.sh
+++ push.sh
@@ -8,13 +8,23 @@ UNIX_DATETIME=$(python3 ota_scripts/helpers/parse_info.py datetime)
 ROSVERSION=$(python3 ota_scripts/helpers/parse_info.py version)
 TELEGRAM_USERNAME=$(python3 ota_scripts/helpers/parse_info.py tgusername)
 
+SOURCELOG=$(cat changelog.txt)
+DEVICELOG=$(cat ${DEVICE}/changelog.txt)
+
 DATETIME=$(date -d @${UNIX_DATETIME})
 
 tg_imagecaptioncast "New RevengeOS update available!" \
-	"Device: ${DEVICENAME} <code>${DEVICE}</code>" \
+	"Device: ${DEVICENAME} (<code>${DEVICE}</code>)" \
 	" " \
 	"RevengeOS Version: ${ROSVERSION}" \
 	"Build date: ${DATETIME}" \
 	" " \
+        "Device changelog:" \
+        "${DEVICELOG}" \
+        " " \
+	"Source changelog:" \
+	"${SOURCELOG}" \
+        " " \
 	"Download link: <a href='${URL}'>${FILENAME}</a>" \
-	"Maintainer: https://t.me/${TELEGRAM_USERNAME}"
+	"Maintainer: https://t.me/${TELEGRAM_USERNAME}" \
+	"Donate: ${DONATE_URL}"
