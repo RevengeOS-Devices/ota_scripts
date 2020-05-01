97be6aeafcd7625646601a8055cebaa3574d72c7
nysascape <jago@nysascape.digital>
2020-05-01 14:37:06 +0000

    push: Cleanup
    Change-Id: I99f9bf3e89803f2b27a981e59d8106874154d095
    Signed-off-by: nysascape <jago@nysascape.digital>

--- push.sh
+++ push.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-LATEST_DEVICE=$DEVICE
-echo $DEVICE
-echo $LATEST_DEVICE
-tg_groupcast "Latest device: <code>${LATEST_DEVICE}</code>"
-
+tg_groupcast "Latest device: <code>${DEVICE}</code>"
