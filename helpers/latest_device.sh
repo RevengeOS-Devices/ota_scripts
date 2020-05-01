83fd1af1b7abd12d21805999652cd4f1a59deb81
nysascape <jago@nysascape.digital>
2020-05-01 13:05:26 +0000

    latest_device: Initial helper
    Change-Id: I407d6814f7eb9b437e3bb424d36d7f81ec716158
    Signed-off-by: nysascape <jago@nysascape.digital>

---
+++ helpers/latest_device.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Grab latest device from official_devices repo
+
+git clone https://github.com/RevengeOS-Devices/official_devices --depth=1 official_devices
+
+DEVICE=$(ls -td official_devices/*/ | head -1)
+echo $DEVICE > device
+sed -i 's|official_devices/||g' device
+sed -i 's|/||g' device
+export DEVICE=$(cat device)
+rm device
+rm -rf official_devices
+echo $DEVICE
