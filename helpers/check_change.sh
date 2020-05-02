db8497791f73c701fc4c374ac6e565f01d7efe52
nysascape <jago@nysascape.digital>
2020-05-02 22:17:07 +0000

    push: Yea doesn't matter
    Change-Id: I28e13c010d0ae25bfc167e5df50313d69ad14b19
    Signed-off-by: nysascape <jago@nysascape.digital>

--- helpers/check_change.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Check if last file was just maintainers.json
-
-CHECK=$(git diff --name-only HEAD~ .)
-if [[ CHECK == "maintainers.json" ]]; then
-	exit 1
-fi
--- push.sh
+++ push.sh
@@ -1,39 +1,45 @@
 #!/bin/bash
 
-URL=$(python3 ota_scripts/helpers/parse_info_device.py url)
-FILENAME=$(python3 ota_scripts/helpers/parse_info_device.py filename)
-DONATE_URL=$(python3 ota_scripts/helpers/parse_info_device.py donate_url)
-UNIX_DATETIME=$(python3 ota_scripts/helpers/parse_info_device.py datetime)
-ROSVERSION=$(python3 ota_scripts/helpers/parse_info_device.py version)
-DEVICENAME=$(python3 ota_scripts/helpers/parse_info_maintainers.py ${DEVICE} name)
-MAINTAINER=$(python3 ota_scripts/helpers/parse_info_maintainers.py ${DEVICE} maintainer)
-TELEGRAM_USERNAME=$(python3 ota_scripts/helpers/parse_info_maintainers.py ${DEVICE} telegram)
-XDA_THREAD=$(python3 ota_scripts/helpers/parse_info_maintainers.py ${DEVICE} xda_thread)
+CHECK=$(git diff --name-only HEAD~ .)
 
-# Make it look pretty
-sed -i -e 's/^/- /g' changelog.txt
-sed -i -e 's/^/- /g' ${DEVICE}/changelog.txt
+if [[ CHECK != "maintainers.json" ]]; then
+	URL=$(python3 ota_scripts/helpers/parse_info_device.py url)
+	FILENAME=$(python3 ota_scripts/helpers/parse_info_device.py filename)
+	DONATE_URL=$(python3 ota_scripts/helpers/parse_info_device.py donate_url)
+	UNIX_DATETIME=$(python3 ota_scripts/helpers/parse_info_device.py datetime)
+	ROSVERSION=$(python3 ota_scripts/helpers/parse_info_device.py version)
+	DEVICENAME=$(python3 ota_scripts/helpers/parse_info_maintainers.py ${DEVICE} name)
+	MAINTAINER=$(python3 ota_scripts/helpers/parse_info_maintainers.py ${DEVICE} maintainer)
+	TELEGRAM_USERNAME=$(python3 ota_scripts/helpers/parse_info_maintainers.py ${DEVICE} telegram)
+	XDA_THREAD=$(python3 ota_scripts/helpers/parse_info_maintainers.py ${DEVICE} xda_thread)
 
-SOURCELOG=$(cat changelog.txt)
-DEVICELOG=$(cat ${DEVICE}/changelog.txt)
+	# Make it look pretty
+	sed -i -e 's/^/- /g' changelog.txt
+	sed -i -e 's/^/- /g' ${DEVICE}/changelog.txt
 
-DATETIME=$(date -d @${UNIX_DATETIME})
+	SOURCELOG=$(cat changelog.txt)
+	DEVICELOG=$(cat ${DEVICE}/changelog.txt)
 
-tg_imagecaptioncast "New RevengeOS update available!" \
-	"Device: ${DEVICENAME} (<code>${DEVICE}</code>)" \
-	"XDA thread: ${XDA_THREAD}" \
-	" " \
-	"RevengeOS Version: ${ROSVERSION}" \
-	"Build date: ${DATETIME}" \
-	" " \
-        "Device changelog:" \
-        "${DEVICELOG}" \
-        " " \
-	"Source changelog:" \
-	"${SOURCELOG}" \
-        " " \
-	"Download link: <a href='${URL}'>${FILENAME}</a>" \
-	"Maintainer: ${MAINTAINER} (@${TELEGRAM_USERNAME})" \
-	"Donate: ${DONATE_URL}"
+	DATETIME=$(date -d @${UNIX_DATETIME})
 
-tg_groupcast "OTA announcement pushed for ${DEVICENAME} (${DEVICE}) in ROS News channel!" \
+	tg_imagecaptioncast "New RevengeOS update available!" \
+		"Device: ${DEVICENAME} (<code>${DEVICE}</code>)" \
+		"XDA thread: ${XDA_THREAD}" \
+		" " \
+		"RevengeOS Version: ${ROSVERSION}" \
+		"Build date: ${DATETIME}" \
+		" " \
+        	"Device changelog:" \
+	        "${DEVICELOG}" \
+        	" " \
+		"Source changelog:" \
+		"${SOURCELOG}" \
+	        " " \
+		"Download link: <a href='${URL}'>${FILENAME}</a>" \
+		"Maintainer: ${MAINTAINER} (@${TELEGRAM_USERNAME})" \
+		"Donate: ${DONATE_URL}"
+
+	tg_groupcast "OTA announcement pushed for ${DEVICENAME} (${DEVICE}) in ROS News channel!" \
+else
+	exit 1
+fi
