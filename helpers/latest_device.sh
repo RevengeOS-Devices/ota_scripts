e5fd2ce11d3d56546072898ec31cec65a6da2a4f
Ethan Halsall <ethan.halsall@gmail.com>
2020-05-07 20:02:35 +0100

    latest_device: fix logic
    Signed-off-by: Ethan Halsall <ethan.halsall@gmail.com>

--- helpers/latest_device.sh
+++ helpers/latest_device.sh
@@ -3,12 +3,16 @@
 
 git clone https://github.com/RevengeOS-Devices/official_devices --depth=1 official_devices
 
-DEVICE=$(ls -drt official_devices/*/ | head -n1)
-echo $DEVICE > device
-sed -i 's|official_devices/||g' device
-sed -i 's|/||g' device
-DEVICE=$(cat device)
-rm device
+cd official_devices
+MESSAGE=$(git log -1 HEAD --pretty=format:%s)
+
+for word in $MESSAGE
+do
+    a+=("$word")
+done
+
+DEVICE=${a[1]}
+cd ..
 rm -rf official_devices
 echo $DEVICE
 export DEVICE
