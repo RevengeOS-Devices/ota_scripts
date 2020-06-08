077a57fd044464683567125130f4b34159954c99
nysascape <jago@nysascape.digital>
2020-06-08 03:18:10 +0100

    latest_device: dumbproof it 100%
    Change-Id: I4783856715548ac36fde382e4aff94d5975ff067
    Signed-off-by: nysascape <jago@nysascape.digital>

--- helpers/latest_device.sh
+++ helpers/latest_device.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 # Grab latest device from official_devices repo
 
-git clone https://github.com/RevengeOS-Devices/official_devices --depth=1 official_devices
+git clone https://github.com/RevengeOS-Devices/official_devices official_devices
 
 cd official_devices
-MESSAGE=$(git log -1 HEAD --pretty=format:%s)
-
-for word in $MESSAGE
-do
-    a+=("$word")
-done
-
-DEVICE=${a[1]}
+LIST=$(git diff-tree --no-commit-id --name-only -r HEAD)
+echo $LIST > temp
+sed 's|/.*||' -i temp
+DEVICE=$(cat temp)
 cd ..
 rm -rf official_devices
 echo $DEVICE
