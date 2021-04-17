b7ca86588725dda26eeb359720f8a491f0a31a5f
Giovix92 <ggualtierone@gmail.com>
2021-04-17 15:34:37 +0200

    ota: ci: added another safety check

--- helpers/latest_device.sh
+++ helpers/latest_device.sh
@@ -4,11 +4,14 @@
 git clone https://github.com/RevengeOS-Devices/official_devices official_devices
 
 cd official_devices
-LIST=$(git diff-tree --no-commit-id --name-only -r HEAD)
-echo $LIST > temp
-sed 's|/.*||' -i temp
+git diff-tree --no-commit-id --name-only -r HEAD | sed 's|/.*||' > temp
 DEVICE=$(cat temp)
+for i in $(cat temp); do
+  if [ "$i" == "maintainers.json" ]; then
+    DEVICE=maintainers.json
+    echo $DEVICE
+  fi
+done
 cd ..
 rm -rf official_devices
 echo $DEVICE
-export DEVICE
