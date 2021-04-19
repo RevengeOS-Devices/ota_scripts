bf445d9b66f1a7f7bb6d0361873a196cc0014f67
Giovix92 <ggualtierone@gmail.com>
2021-04-19 19:12:42 +0200

    ota: ci: rework maintainers.json check

--- helpers/latest_device.sh
+++ helpers/latest_device.sh
@@ -11,17 +11,17 @@ git clone https://github.com/RevengeOS-Devices/official_devices official_devices
 cd official_devices
 LIST=$(git diff-tree --no-commit-id --name-only -r HEAD)
 echo $LIST > temp
-sed 's|/.*||' -i temp
-DEVICE=$(cat temp)
 for i in $(cat temp); do
   if [ "$i" == "maintainers.json" ]; then
-    DEVICE=maintainers.json
+    DEVICE="maintainers.json"
     echo $DEVICE
-    export $DEVICE
+    export DEVICE="maintainers.json"
     cleanup
     exit
   fi
 done
+sed 's|/.*||' -i temp
+DEVICE=$(cat temp)
 cleanup
 echo $DEVICE
 export DEVICE
